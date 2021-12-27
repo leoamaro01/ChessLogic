b816ce26cbf09b652c0edd1cd0f518b6652ebec8
PixelDG <35879142+pixeldashgames@users.noreply.github.com>
2021-12-26 19:45:22 -0500

    Added: New Exception type for handling validity problems.

--- ChessLogic/Exceptions.cs
+++ ChessLogic/Exceptions.cs
@@ -15,4 +15,14 @@ namespace PixelDashCore.ChessLogic
             System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
+    [System.Serializable]
+    public class InvalidPlaceException : System.Exception
+    {
+        public InvalidPlaceException() { }
+        public InvalidPlaceException(string message) : base(message) { }
+        public InvalidPlaceException(string message, System.Exception inner) : base(message, inner) { }
+        protected InvalidPlaceException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
 }
